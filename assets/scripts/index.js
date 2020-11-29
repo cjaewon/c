1710ee7c2180559c6d08f58618444805ba1dd768
cjaewon <jwn4492@outlook.com>
2020-11-29 22:06:18 +0900

    Add Description && Add Google gtag

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,10 @@
 import turbolinks from 'turbolinks';
 
 turbolinks.start();
+
+document.addEventListener("turbolinks:load", e => {
+  if (typeof window.ga === 'function') {
+    window.ga('set', 'page', e.data.url);
+    window.ga('send', 'pageview');
+  }
+});
